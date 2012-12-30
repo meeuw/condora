4db04caeac975b12335bb7ad4b701db544a0305a
Dick Marinus <dick@mrns.nl>
2012-12-30 15:15:23 +0100

    changes

--- condora-policy.py
+++ condora-policy.py
@@ -15,7 +15,7 @@ class _RpmPolicy(policy.PackagePolicy):
 	if 'includes' in keywords: self.includes.append(keywords.pop('includes'))
 	if 'excludes' in keywords: self.excludes.append(keywords.pop('excludes'))
 	policy.PackagePolicy.updateArgs(self, **keywords)
-    def doRpm(self, recipe, args):
+    def doRpm(self, recipe, queryformat):
         if not self.includes: return
         rpmFileNames = []
         for include in self.includes:
@@ -25,7 +25,7 @@ class _RpmPolicy(policy.PackagePolicy):
                 rpmFileNames.remove(rpmFileName)
         ret = []
         for rpmFileName in rpmFileNames:
-            ret += util.popen('%s %s' % (args, rpmFileName)).read().split('\n')
+            ret += util.popen('rpm -qp --queryformat \'%s\' %s' % (queryformat, rpmFileName)).read().split('\n')
         return ret
     
 class RpmRequires(_RpmPolicy):
@@ -35,7 +35,7 @@ class RpmRequires(_RpmPolicy):
         ('Requires', policy.REQUIRED_SUBSEQUENT),
     )
     def doProcess(self, recipe):
-        for requires in self.doRpm(recipe, 'rpm -qp --queryformat \'[%{NAME},%{REQUIRES}\\n]\''):
+        for requires in self.doRpm(recipe, '[%{NAME},%{REQUIRES}\\n]'):
             requires_split = requires.split(',')
             if len(requires_split) >= 2:
                 req = requires_split[1]
@@ -53,7 +53,7 @@ class RpmFiles(_RpmPolicy):
         ('PackageSpec', policy.REQUIRED_SUBSEQUENT),
     )
     def doProcess(self, recipe):
-        for files in self.doRpm(recipe, 'rpm -qp --queryformat \'[%{NAME},0%{FILEMODES:octal},%{FILEUSERNAME},%{FILEGROUPNAME},%{FILENAMES}\\n]\''):
+        for files in self.doRpm(recipe, '[%{NAME},0%{FILEMODES:octal},%{FILEUSERNAME},%{FILEGROUPNAME},%{FILENAMES}\\n]'):
             files_split = files.split(',')
             if len(files_split) >= 5:
                 name, perms, owner, group, target = files_split
@@ -64,10 +64,57 @@ class RpmFiles(_RpmPolicy):
 class RpmScripts(_RpmPolicy):
     bucket = policy.PACKAGE_CREATION
     processUnmodified = False
+    requires = (
+        ('PackageSpec', policy.REQUIRED_SUBSEQUENT),
+    )
     def doProcess(self, recipe):
-        print self.doRpm('rpm -qp '+myrpmfilename+' --queryformat \'' \
-                 '        update)\n        %%{POSTIN}\\n        %%{POSTINPROG}\\n        ;;\\n' \
-                 '        preremove)\n        %%{PREUN}\\n        %%{PREUNPROG}\\n        ;;\\n' \
-                 '        remove)\n        %%{POSTUN}\\n        %%{POSTUNPROG}\\n        ;;\\n' \
-                 '        preupdate)\n        %%{PREIN}\\n        %%{PREINPROG}\\n        ;;\\n' \
-                 '\'|grep -v \'^        (none)$\' >> %(destdir)s/%(taghandlerdir)s/'+package, package=package)
+        m = {}
+        rpm2conary = {'POSTIN':'update','PREUN':'preremove','POSTUN':'remove','PREIN':'preupdate'}
+        conaryactions = []
+        for querytag in ['POSTIN','POSTINPROG','PREUN','PREUNPROG','POSTUN','POSTUNPROG','PREIN','PREINPROG']:
+            ret = self.doRpm(recipe, '%%{NAME}\n%%{VERSION}\n%%{RELEASE}\n%%{%s}' % querytag)
+            if '(none)' in ret: ret.remove('(none)')
+            name, version, release = ret[:3]
+            ret = ret[3:]
+            if not name in m: m[name] = {'NAME':name,'VERSION':version,'RELEASE':release}
+            if not 'SCRIPTS' in m[name]: m[name]['SCRIPTS'] = ''
+            if ret:
+                for rpm, conary in rpm2conary.itervalues:
+                    if querytag.startswith(rpm): break
+                conaryactions += conary
+                m[name]['SCRIPTS'] += '''%s)
+%s
+;;''' % (conary, '\n'.join(ret))
+        if conaryactions:
+            for name, scripts in m.iteritems():
+                os.makedirs('%(destdir)s/%(taghandlerdir)s' % recipe.macros)
+                taghandlerfn = ('%(destdir)s/%(taghandlerdir)s/' % recipe.macros) + name
+                with file(taghandlerfn,'w') as f: f.write('''#!/bin/bash
+# %(NAME)s-%(VERSION)s.%(RELEASE)s
+if [ $# -lt 2 ]; then
+    echo "not enough arguments: $0 $*" >&2
+    exit 1
+fi
+
+type="$1"
+shift
+action="$1"
+shift
+case $type in
+    files)
+        case $action in
+%(SCRIPTS)s
+        esac
+    ;;
+esac
+''' % scripts)
+                os.chmod(taghandlerfn, 0755)
+                recipe.PackageSpec(name, taghandlerfn)
+                tagdescriptionfn = ('%(destdir)s/%(tagdescriptiondir)s/' % recipe.macros) + name
+                os.makedirs('%(destdir)s/%(tagdescriptiondir)s' % recipe.macros)
+                with file(tagdescriptionfn,'w') as f:
+                    f.write('file\t%s/%s\ndatasource\tstdin\n' % (recipe.macros.taghandlerdir, name))
+                    for conaryaction in conaryactions:
+                        f.write('implements\tfiles\n'+conaryaction)
+                    f.write('include\t%s/%s' % (recipe.macros.taghandlerdir, name))
+                recipe.PackageSpec(name, tagdescriptionfn)
